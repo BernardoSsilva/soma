fae957564db8d0e11a65c08626646a2e0db9b531
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-03-16 18:16:33 -0300

    Mudança de condicional
    substituida a tentativa de uso de if para utilizar try em conjunto do bloco catch

--- EXERCICIOGITHUB/soma.java
+++ EXERCICIOGITHUB/soma.java
@@ -4,23 +4,33 @@ import java.util.Scanner;
 
 public class soma{
     public static void main(String[] args) {
-        int soma, val1Int, val2Int
-        String valor1, valor2, soma;
+        int soma, val1Int, val2Int;
+        String valor1, valor2;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
-        valor1 = scan.next();
-        if(valor1.matches("[0-9] +")){
+
+        // tentar executar o codigo dentro de try
+        try {
+            System.out.println("Digite um numero: ");
+            valor1 = scan.next();
             val1Int = Integer.parseInt(valor1);
-            System.out.println("Digite outro numero: ");
-            valor2= scan.next();
-            if(valor2.matches("[0-9]+")){
-                val2Int = Integer.parseInt(valor2)
+
+            // tentar executar o codigo dentro de try
+            try {
+                System.out.println("Digite outro numero: ");
+                valor2= scan.next();
+                val2Int = Integer.parseInt(valor2);
                 soma = val1Int+val2Int;
                 System.out.println("O resultado é: "+soma);
+
+             // caçar uma exceção, caso não for um valor inteiro como o determinado nas variaveis encerrar execução    
+            } catch (Exception e){
+                //o bloco catch procura entradas invalidas e as toma como excessão e encerra o funcionamento
+                System.out.println("Valor invalido, Somente numeros são aceitos");
             }
-            
-        }else{
-            System.out.println("Valor Invalido");
+
+            // caçar uma exceção, caso não for um valor inteiro como o determinado nas variaveis encerrar excecução 
+        }catch(Exception e){
+            System.out.println("Valor invalido, Somente numeros são aceitos");
         }
         
     }
