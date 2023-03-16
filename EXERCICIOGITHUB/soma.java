a86817521ff9b8df65a2a9cae4afa50266bea786
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-03-16 09:15:33 -0300

    Add files via upload

--- EXERCICIOGITHUB/soma.java
+++ EXERCICIOGITHUB/soma.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 public class soma{
     public static void main(String[] args) {
-        int valor1, valor2, soma;
+        int soma, val1Int, val2Int
+        String valor1, valor2, soma;
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite um numero: ");
-        valor1 = scan.nextInt();
-        System.out.println("Digite outro numero: ");
-        valor2 = scan.nextInt();
-        soma = valor1+valor2;
-        System.out.println("O resultado é: "+soma);
+        valor1 = scan.next();
+        if(valor1.matches("[0-9] +")){
+            val1Int = Integer.parseInt(valor1);
+            System.out.println("Digite outro numero: ");
+            valor2= scan.next();
+            if(valor2.matches("[0-9]+")){
+                val2Int = Integer.parseInt(valor2)
+                soma = val1Int+val2Int;
+                System.out.println("O resultado é: "+soma);
+            }
+            
+        }else{
+            System.out.println("Valor Invalido");
+        }
+        
     }
 }
